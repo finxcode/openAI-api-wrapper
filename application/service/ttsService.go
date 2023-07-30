bb478ebdfbde7e5442ba0b615f14b0a6c63c1ce6
finxcode <f.sheng@outlook.com>
2023-07-30 17:09:54 +0800

    tts response timeout +5

--- application/service/ttsService.go
+++ application/service/ttsService.go
@@ -4,6 +4,7 @@ import (
 	"chatGPT-api-wrapper/application/port/in/request"
 	"chatGPT-api-wrapper/application/port/in/response"
 	"chatGPT-api-wrapper/application/port/out/tts"
+	"errors"
 )
 
 type TtsService struct {
@@ -18,5 +19,8 @@ func NewTtsService(ttsPort tts.Port) *TtsService {
 
 func (t *TtsService) GetTts(command request.TtsCommand) (response.TtsResponse, error) {
 	ttsResp, err := t.ttsPort.GetTtsResult(&command)
+	if ttsResp == nil {
+		return response.TtsResponse{}, errors.New("timeout")
+	}
 	return *ttsResp, err
 }
